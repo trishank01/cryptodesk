4ab22d6720c7f68d5f1d770c2fbd23c5dc850c2a
tk <trishank01@gmail.com>
2022-11-29 22:03:19 +0530

    added navbar with react-router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,45 @@
+import React from "react";
+import { Routes, Route, Link } from 'react-router-dom'
+import { Navbar, Homepage, Exchanges, Cryptocurrencies, CryptoDetails, News } from "./components";
+import "./App.css"
+import { Layout, Space, } from "antd";
+import Typography from "antd/es/typography/Typography";
+
+
+
 function App() {
+
   return (
-    <h1 className="text-3xl bg-slate-500  font-bold underline">
-      Hello world!
-    </h1>
+    <div className="app">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="main">
+        <Layout>
+          <div className="routes">
+            <Routes>
+              <Route exect path="/" element={<Homepage />} />
+              <Route path="/exchanges" element={<Exchanges />} />
+              <Route exect path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              <Route exect path="/crypto/coinId" element={<CryptoDetails />} />
+              <Route exect path="/news" element={<News />} />
+            </Routes>
+          </div>
+        </Layout>
+      
+      <div className="footer" >
+         <Typography.Title level={5} style={{color:"white" , textAlign:"center"}}>
+          CryptoDesk <br/> 
+          All rights reservered
+         </Typography.Title>
+         <Space>
+          <Link to="/">Home</Link>
+          <Link to="/exchanges">Exchanges</Link>
+          <Link to="/news">News</Link>
+         </Space>
+      </div>
+      </div>
+    </div>
   );
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export {default as Navbar } from './Navbar'
+export {default as Homepage } from '../pages/Homepage'
+export {default as Exchanges } from '../pages/Exchanges'
+export {default as Cryptocurrencies } from '../pages/Cryptocurrencies'
+export {default as CryptoDetails } from '../pages/CryptoDetails'
+export {default as News } from '../pages/News'
